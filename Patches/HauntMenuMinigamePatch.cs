bcc9733c02250676364bea526c04743a6bd00f47
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2023-03-28 13:21:31 +0900

    feature: 憑依時にデフォルトで生存者にフィルタする設定

---
+++ Patches/HauntMenuMinigamePatch.cs
@@ -0,0 +1,17 @@
+using HarmonyLib;
+
+namespace TownOfHost.Patches
+{
+    [HarmonyPatch]
+    public static class HauntMenuMinigamePatch
+    {
+        [HarmonyPatch(typeof(HauntMenuMinigame), nameof(HauntMenuMinigame.Begin)), HarmonyPostfix]
+        public static void BeginPostfix(HauntMenuMinigame __instance)
+        {
+            if (Main.HauntMenuFocusCrewmate.Value && __instance.filterMode != HauntMenuMinigame.HauntFilters.Crewmate)
+            {
+                __instance.SetFilter((int)HauntMenuMinigame.HauntFilters.Crewmate);
+            }
+        }
+    }
+}
