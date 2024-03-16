ee7de666e83b64904cd99027b2666df5f0009763
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2024-03-16 20:34:50 +0900

    version 2024.3.5.0

--- main.cs
+++ main.cs
@@ -54,7 +54,7 @@ namespace TownOfHost
         // ==========
         //Sorry for many Japanese comments.
 
-        public static readonly string ForkVersion = "2023.11.28.0";
+        public static readonly string ForkVersion = "2024.3.5.0";
         public static readonly Version ParsedForkVersion = Version.Parse(ForkVersion);
 
         public const string PluginGuid = "com.emptybottle.townofhost";
