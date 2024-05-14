2840331c169d6e1edbf509ff6d84486c619f1c99
Hyz-sui <86903430+Hyz-sui@users.noreply.github.com>
2024-05-15 03:28:21 +0900

    feature: new colors

--- Modules/CustomPalette.cs
+++ Modules/CustomPalette.cs
@@ -6,5 +6,7 @@ namespace TownOfHost.Modules
     {
         public static readonly Color32 EnabledGreen = new(0, 255, 127, 255);
         public static readonly Color32 DisabledRed = new(255, 99, 71, 255);
+        public static readonly Color32 SucceededBlue = new(0, 144, 255, 255);
+        public static readonly Color32 FailedRed = new(255, 0, 148, 255);
     }
 }
